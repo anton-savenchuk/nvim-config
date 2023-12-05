4dc9f82ab92cd90278504b635410a63d796e00a2
Anton Savenchuk <Anton.Savenchuk@gmail.com>
2023-12-05 10:10:31 +0700

    feat: add debugging plugin

--- plugins.lua
+++ plugins.lua
@@ -90,6 +90,42 @@ local plugins = {
         },
     },
 
+    -- Debugging
+    {
+        "rcarriga/nvim-dap-ui",
+        init = function()
+            require("core.utils").load_mappings "Dap"
+        end,
+        dependencies = {
+            {
+                "mfussenegger/nvim-dap",
+                config = function()
+                    -- NOTE: Check out this for guide
+                    -- https://github.com/mfussenegger/nvim-dap/wiki/Debug-Adapter-installation
+                    local dap = require "dap"
+                    vim.fn.sign_define("DapBreakpoint",
+                        { text = "", texthl = "DiagnosticSignError", linehl = "", numhl = "" })
+
+                    local dapui = require "dapui"
+                    dap.listeners.after.event_initialized["dapui_config"] = function()
+                        dapui.open()
+                    end
+
+                    -- dap.listeners.before.event_terminated["dapui_config"] = function()
+                    --   dapui.close()
+                    -- end
+
+                    -- dap.listeners.before.event_exited["dapui_config"] = function()
+                    --   dapui.close()
+                    -- end
+
+                    -- NOTE: Make sure to install the needed files/exectubles through mason
+                    require "custom.configs.dap.settings.debugpy"
+                end,
+            },
+        },
+        opts = require "custom.configs.dap.ui",
+    },
 }
 
 return plugins
