1304793eec149f093caf4bbcbb367024c8840b84
Anton Savenchuk <Anton.Savenchuk@gmail.com>
2023-12-05 10:54:21 +0700

    mason settings update

--- configs/mason.lua
+++ configs/mason.lua
@@ -1,23 +1,37 @@
 return {
-  ui = {
-    -- Whether to automatically check for new versions when opening the :Mason window.
-    check_outdated_packages_on_open = false,
-    icons = {
-      package_pending = " ",
-      package_installed = " ",
-      package_uninstalled = " ",
+    ui = {
+        -- Whether to automatically check for new versions when opening the :Mason window.
+        check_outdated_packages_on_open = false,
+        icons = {
+            package_pending = " ",
+            package_installed = " ",
+            package_uninstalled = " ",
+        },
     },
-  },
-  ensure_installed = {
-    -- Lua
-    "lua-language-server",
-    "vim-language-server",
-    "stylua",
+    ensure_installed = {
+        -- Lua
+        "lua-language-server",
+        "vim-language-server",
+        "stylua",
 
-    -- Python
-    "pyright",
-    "black",
-    "flake8",
-    "ruff",
-  },
+        --Web Development
+        "prettier",
+        "css-lsp",
+        "html-lsp",
+        "typescript-language-server",
+        "emmet-language-server",
+
+        -- Python
+        "pyright",
+        "black",
+        "flake8",
+        "ruff",
+
+        -- Yaml
+        "yaml-language-server",
+
+        -- Docker
+        "docker_compose_language_service",
+        "dockerls",
+    },
 }
